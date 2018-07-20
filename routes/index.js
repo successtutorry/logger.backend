d21d7f05681f2f59e9974c176cf3a9d76f4eea59
surendrap720 <surendrap720@gmail.com>
2018-07-20 16:15:21 +0530

    Changes home page to show requirements of students

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,28 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', (req, res) => {
+const Requirement = require('../models/requirements');
+
+
+/*router.get('/', (req, res) => {
+
+
     res.render('index');
-});
+});*/
+
+router.get( (req, res, next) => {
+    Requirement.find( function(err, docs){
+      var requirementChunks = [];
+      var chunkSize = 3;
+      for(var i=0; i < docs.length; i+= chunkSize){
+          requirementChunks.push(docs.slice(i, i+chunkSize));
+      }
+        res.render('index', {  requirement: requirementChunks });
+      
+     
+      // username: req.user.username,
+     // email: req.user.email
+    });
+  });
 
 module.exports = router;
