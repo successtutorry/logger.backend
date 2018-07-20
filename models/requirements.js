458843ead2a273e261a499e36b46b618ad38b492
surendrap720 <surendrap720@gmail.com>
2018-07-20 16:14:33 +0530

    Requirement model created

---
+++ models/requirements.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+
+
+const requirementSchema = new Schema({
+
+	name: String,
+	age: String,
+	class: String,
+	subject: String,
+	email: String
+	
+
+}, {
+
+	timestamps: { // this will give us the detail when the requiremnt is created
+
+		createdAt: 'createdAt',
+		updatedAt: 'updatedAt'
+	}
+});
+
+
+const Requirement = mongoose.model('requirement', requirementSchema);
+module.exports = Requirement;
