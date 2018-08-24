de35153556758977c733c9453760af4a26d66895
surendrap720 <surendrap720@gmail.com>
2018-08-24 16:26:19 +0530

    latest commit 24th august

--- app.js
+++ app.js
@@ -1,44 +1,11 @@
-
-// this is the main page of our app
-
-
 const express = require('express');
 const morgan = require('morgan')
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const expressHandlebars = require('express-handlebars');
-const flash = require('connect-flash');
-const session = require('express-session');
-const mongoose = require('mongoose');
-const passport  = require('passport');
-//const mongoStore = require('connect-mongo');
-
-require('./config/passport'); 
-
-
-
-mongoose.Promise = global.Promise;
-
-//database connection 
-
-
-mongoose.connect('mongodb://104.183.242.32:27017/tutorry', { useNewUrlParser: true}, function(err){
-	if(err){
-		console.log(err);
-	} else {
-		console.log('Connected to the database');
-	}
-});
-
-
-
 const app = express();
-
-// middleware which logs all the details to the console
 app.use(morgan('dev'));
-
-
 
 // View Engine 
 app.set('views', path.join(__dirname, 'views'));
@@ -48,40 +15,9 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(session({
-  cookie: { maxAge: 60000 },
-  secret: 'codeworkrsecret',
-  saveUninitialized: false,
-  resave: false,
-  //store: new mongoStore({ mongooseConnection: mongoose.connection}),
-}));
 
-
-
-app.use(passport.initialize());
-app.use(passport.session());
-app.use(flash());
-
-// new middleware
-app.use((req, res, next) => {
-
-	res.locals.success_messages = req.flash('success');
-	res.locals.error_messages = req.flash('error');
-	res.locals.isAuthenticated = req.user ? true: false;
-	next();
-});
-
-
-
-// to allow to pass the routes through these
 
 app.use('/', require('./routes/index'));
 app.use('/users', require('./routes/users'));
-
-// catch 404 and forward to error handler
-app.use((req, res, next) => {
-
-  res.render('notFound');
-});
 
 app.listen(4000, () => console.log('Server started listening on port 4000!'));
--- routes/index.js
+++ routes/index.js
@@ -1,42 +1,10 @@
 
-
 const express = require('express');
 const router = express.Router();
 
-const Requirement = require('../models/requirements');
-const User = require('../models/user');
-
-
-
 router.get('/', (req, res) => {
-
-
-    res.render('index');
+	res.render('index');
 });
 
-/*router.get( '/', (req, res, next) => {
-    Requirement.find( function(err, docs){
-      var requirementChunks = [];
-      var chunkSize = 3;
-      for(var i=0; i < docs.length; i+= chunkSize){
-          requirementChunks.push(docs.slice(i, i+chunkSize));
-      }
-        res.render('index', {  requirement: requirementChunks });
-     
-    });*/
-
-    /* User.find( function(err, docs){
-      var tutorChunks = [];
-      var chunkSize = 3;
-      for(var i=0; i < docs.length; i+= chunkSize){
-          tutorChunks.push(docs.slice(i, i+chunkSize));
-      }
-       // res.render('index', {  tutor: tutorChunks });
-       res.render('index', {  tutor: tutorChunks  });
-     });*/
-
-		//res.render('index', {  tutor: tutorChunks , requirement: requirementChunks });     
-
-  
 
 module.exports = router;
