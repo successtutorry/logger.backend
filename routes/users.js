4172982f09ea0007e1a3c5d047b39dcaad071dbf
surendrap720 <surendrap720@gmail.com>
2018-09-08 16:24:04 +0530

    basic navigation api for login, register created

--- routes/users.js
+++ routes/users.js
@@ -14,14 +14,35 @@ router.route('/inner')
 router.route('/find_tutor')
   .get((req, res) => {
     res.render('find_tutor');
-  })
+  });
 
 router.route('/become_tutor')
   .get((req, res) => {
     res.render('become_tutor');
-  })
+  });
   router.route('/contacts')
   .get((req, res) => {
     res.render('contacts');
-  })
+  });
+
+  router.route('/find_a_tutor')
+  .post((req,res) =>{
+
+    console.log(req.body);
+    res.render('find_tutor');
+    return;
+  });
+
+  router.route('/login')
+  .get((req,res) =>{
+
+    res.render('login');
+  });
+
+ router.route('/register')
+  .get((req,res) =>{
+
+    res.render('register');
+  });
+
 module.exports = router;
