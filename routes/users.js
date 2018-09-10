5e318f84770b69452b836f06d7405a0ba80c6b51
surendrap720 <surendrap720@gmail.com>
2018-09-10 15:43:52 +0530

    post api for contact form added

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,24 @@
 const express = require('express');
 const router = express.Router();
+const Joi = require('joi');
+
+const ContactusForm = require('../models/contactusform');
+
+
+
+// Validation Schema, validationg the fields which user is entering
+
+const contactusSchema = Joi.object().keys({
+  email: Joi.string().email().required(),
+  name: Joi.string().required(),
+  message: Joi.string().required()
+  
+});
+
+
+
+
+
 
 
 router.route('/inner')
@@ -14,14 +33,33 @@ router.route('/inner')
 router.route('/find_tutor')
   .get((req, res) => {
     res.render('find_tutor');
-  })
+  });
 
 router.route('/become_tutor')
   .get((req, res) => {
     res.render('become_tutor');
-  })
+  });
+
   router.route('/contacts')
   .get((req, res) => {
     res.render('contacts');
-  })
+  }).post((req, res) => {
+    
+      const result = Joi.validate(req.body, contactusSchema);
+      if (result.error) {
+          console.log(result.error);
+        
+        res.redirect('/users/contacts');
+      
+      }
+
+      const newContact = new ContactusForm(result.value); 
+      console.log('newContact', newContact);
+      newContact.save();
+      res.redirect('/users/contacts');
+
+
+  });
+
+
 module.exports = router;
